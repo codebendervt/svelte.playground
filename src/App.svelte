0119ea99a7fbde5778f2e9df8425e3e768a61ec4
rawkthethinker <47385943+rawkthethinker@users.noreply.github.com>
2020-03-06 16:01:33 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+import Nav from './components/nav.svelte';
+import About from './components/about.svelte';
+import Portfolio from './components/portfolio.svelte'
+
+</script>
+
+
+<Nav/>
+<About/>
+<Portfolio/>
---
+++ src/components/nav.svelte
@@ -0,0 +1,70 @@
+<script>
+let backgroundImage = "/img/background1.jpg";
+</script>
+
+
+<header id="home">
+
+<div class="bg-img" style="background-image: url({backgroundImage} );">
+<div class="overlay"></div>
+</div>
+
+
+<nav id="nav" class="navbar nav-transparent">
+<div class="container">
+<div class="navbar-header">
+
+<div class="navbar-brand">
+<a href="index.html">
+<img class="logo" src="img/logo.png" alt="logo">
+<img class="logo-alt" src="img/logo-alt.png" alt="logo">
+</a>
+</div>
+
+
+<div class="nav-collapse">
+<span></span>
+</div>
+
+</div>
+
+<ul class="main-nav nav navbar-nav navbar-right">
+<li><a href="#home">Home</a></li>
+<li><a href="#about">About</a></li>
+<li><a href="#portfolio">Portfolio</a></li>
+<li><a href="#service">Services</a></li>
+<li><a href="#pricing">Prices</a></li>
+<li><a href="#team">Team</a></li>
+<li class="has-dropdown"><a href="#blog">Blog</a>
+<ul class="dropdown">
+<li><a href="blog-single.html">blog post</a></li>
+</ul>
+</li>
+<li><a href="#contact">Contact</a></li>
+</ul>
+
+</div>
+</nav>
+
+
+<div class="home-wrapper">
+<div class="container">
+<div class="row">
+
+<div class="col-md-10 col-md-offset-1">
+<div class="home-content">
+<h1 class="white-text">We Are Creative Agency</h1>
+<p class="white-text">Morbi mattis felis at nunc. Duis viverra diam non justo. In nisl. Nullam sit amet magna in magna gravida vehicula. Mauris tincidunt sem sed arcu. Nunc posuere.
+</p>
+<button class="white-btn">Get Started!</button>
+<button class="main-btn">Learn more</button>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+</header>
+
+
